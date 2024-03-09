4b37faf4eb9b19352f94c0e931ba51245dd906bb
edubilim <162502604+edubilim@users.noreply.github.com>
2024-03-09 15:50:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,3 @@ function showTab(tabId) {
     var activeTab = document.querySelector('nav a[onclick="showTab(\'' + tabId + '\')"]');
     activeTab.classList.add('active');
 }
-
-function toggleAnswer(faqNumber) {
-    var answer = document.querySelector('.faq-item:nth-child(' + faqNumber + ') .answer');
-    answer.style.display = (answer.style.display === 'none' || answer.style.display === '') ? 'block' : 'none';
-}
